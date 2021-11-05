c829281c4ff20e714c8398c3dbfb0ef8d6707ff2
highgoal215 <highgoal215@gmail.com>
2021-11-05 13:30:55 +0100

    minor changes

--- Ticket.sol
+++ Ticket.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/openzeppelin-contracts/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Ticket is ERC721{
     using Counters for Counters.Counter;
